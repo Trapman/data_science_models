c924b8d51f029738e0fad3ef318098e6ad77f86c
Trapman <david.a.shuttleworth@gmail.com>
2019-11-09 21:25:02 -0500

    Create python.py

---
+++ python.py
@@ -0,0 +1,4 @@
+#anaconda for pre-installed packages
+#pip: which is a python package manager that allows you to easily install 3rd party packages
+#iPython: much nicer python shell to work with. 
+#anaconda comes with both
