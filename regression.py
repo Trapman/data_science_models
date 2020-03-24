f69dafcd312dde6d51df3e4d154b99222131d0d9
Trapman <david.a.shuttleworth@gmail.com>
2020-03-23 20:56:12 -0400

    Create regression.py

---
+++ regression.py
@@ -0,0 +1,19 @@
+# https://towardsdatascience.com/simple-and-multiple-linear-regression-in-python-c928425168f9
+
+#  Linear regression is a statistical model that examines the linear relationship between two (Simple Linear Regression ) or more 
+# (Multiple Linear Regression) variables— a dependent variable and independent variable(s). 
+# Linear relationship basically means that when one (or more) independent variables increases (or decreases), the dependent variable 
+# increases (or decreases) too. 
+
+# A relationship between variables Y and X is represented by this equation:   Y`i = mX + b
+
+# In most cases, we will have more than one independent variable — we’ll have multiple variables; it can be as little as 
+# two independent variables and up to hundreds (or theoretically even thousands) of variables. 
+# In those cases we will use a Multiple Linear Regression model (MLR). The regression equation is pretty much the same as the 
+# simple regression equation, just with more variables:
+# Y’i = b0 + b1X1i + b2X2i
+
+
+# Linear Regression in Statsmodels
+Statsmodels is “a Python module that provides classes and functions for the estimation of many different statistical models, 
+# as well as for conducting statistical tests, and statistical data exploration.” 
